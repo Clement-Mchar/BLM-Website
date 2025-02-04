40728c6a701d6319f9fe5d3832e642561e729736
Clement-Mchar <clement.mchar@gmail.com>
2025-02-04 10:31:01 +0100

    added the dashboard vue component

--- BLM_FRNT/src/router/index.ts
+++ BLM_FRNT/src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Login from '../views/Login.vue';
+import Login from '~/views/Login.vue';
+import Dashboard from '~/views/Dashboard.vue';
 
 const routes = [
   {
     path: '/login',
     name: 'Login',
     component: Login,
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
   },
 ];
 
