e5220b63d2aea62c6e33adbad7e07d7ed967c105
Clement-Mchar <clement.mchar@gmail.com>
2025-02-12 16:29:37 +0100

    undoing a wrong commit

--- Backup/BLM_BCK/app/controllers/user_controller.ts
+++
@@ -1,7 +0,0 @@
-import type { HttpContext } from '@adonisjs/core/http'
-
-export default class UserController {
-  async getCurrentUser({ auth }: HttpContext) {
-    return auth.getUserOrFail()
-  }
-}
--- Backup/BLM_BCK/app/enums.ts
+++
@@ -1,6 +0,0 @@
-enum UserRole {
-    Admin = 'ADMIN',
-    Webmaster = 'WEBMASTER'
-  }
-
-  export { UserRole }
