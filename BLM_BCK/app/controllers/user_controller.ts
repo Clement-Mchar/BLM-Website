73dedb52cc2847a91b1e64c2fab37965ab4064e2
Clement-Mchar <clement.mchar@gmail.com>
2025-03-19 23:00:03 +0100

    added a create method to user_controller and user_service

--- BLM_BCK/app/controllers/user_controller.ts
+++ BLM_BCK/app/controllers/user_controller.ts
@@ -1,14 +1,14 @@
 import { inject } from '@adonisjs/core'
 import { UserService } from '#services/user_service'
-
 @inject()
 export default class UserController {
-  constructor(
-    private userService: UserService,
-  ) {}
+  constructor(private userService: UserService) {}
 
   async index() {
     return this.userService.all()
   }
 
+  async create() {
+    return this.userService.create()
+  }
 }
--- BLM_BCK/app/services/user_service.ts
+++ BLM_BCK/app/services/user_service.ts
@@ -1,9 +1,25 @@
-import User from "#models/user"
+import User from '#models/user'
+import { HttpContext } from '@adonisjs/core/http'
+import { UserRole } from '../enums.js'
+import { inject } from '@adonisjs/core'
 
+@inject()
 export class UserService {
+  constructor(private ctx: HttpContext) {}
+  async all() {
+    const users = await User.all()
+    return users
+  }
 
-    async all() {
-        const users = await User.all()
-        return users
-    }
+  async create() {
+    const user = new User()
+    const role = this.ctx.request.input('role')
+    if (!Object.values(UserRole).includes(role)) throw new Error('Invalid role')
+    user.username = this.ctx.request.input('username')
+    user.password = this.ctx.request.input('password')
+    user.userRole = role
+    user.isAdmin = role === UserRole.Admin
+    await user.save()
+    return user
+  }
 }
