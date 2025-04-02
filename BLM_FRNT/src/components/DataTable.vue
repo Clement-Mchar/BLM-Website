7ec6275d84dc31a0c3dc4ba04ebf4125fc8c0261
Clement-Mchar <clement.mchar@gmail.com>
2025-04-02 20:13:46 +0200

    added pagination and sorting to user's list

--- BLM_FRNT/src/components/DataTable.vue
+++ BLM_FRNT/src/components/DataTable.vue
@@ -1,53 +1,81 @@
 <template>
-  <div class="border rounded-md">
-    <Table>
-      <TableHeader>
-        <TableRow
-          v-for="headerGroup in table.getHeaderGroups()"
-          :key="headerGroup.id"
-        >
-          <TableHead
-            class="text-white"
-            v-for="header in headerGroup.headers"
-            :key="header.id"
-          >
-            <FlexRender
-              v-if="!header.isPlaceholder"
-              :render="header.column.columnDef.header"
-              :props="header.getContext()"
-            />
-          </TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <template v-if="table.getRowModel().rows?.length">
+  <div class="flex flex-col">
+    <div
+      class="flex flex-col border-r-0 border-l-0 border-t-2 border-b-2 max-w-fit h-fit justify-center"
+    >
+      <Table>
+        <TableHeader>
           <TableRow
-            v-for="row in table.getRowModel().rows"
-            :key="row.id"
-            :data-state="row.getIsSelected() ? 'selected' : undefined"
+            v-for="headerGroup in table.getHeaderGroups()"
+            :key="headerGroup.id"
           >
-            <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
+            <TableHead
+              class="text-white"
+              v-for="header in headerGroup.headers"
+              :key="header.id"
+            >
               <FlexRender
-                :render="cell.column.columnDef.cell"
-                :props="cell.getContext()"
+                v-if="!header.isPlaceholder"
+                :render="header.column.columnDef.header"
+                :props="header.getContext()"
               />
-            </TableCell>
+            </TableHead>
           </TableRow>
-        </template>
-        <template v-else>
-          <TableRow>
-            <TableCell :colspan="columns.length" class="h-24 text-center">
-              No results.
-            </TableCell>
-          </TableRow>
-        </template>
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          <template v-if="table.getRowModel().rows?.length">
+            <TableRow
+              v-for="row in table.getRowModel().rows"
+              :key="row.id"
+              :data-state="row.getIsSelected() ? 'selected' : undefined"
+            >
+              <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
+                <FlexRender
+                  :render="cell.column.columnDef.cell"
+                  :props="cell.getContext()"
+                />
+              </TableCell>
+            </TableRow>
+          </template>
+          <template v-else>
+            <TableRow>
+              <TableCell :colspan="columns.length" class="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          </template>
+        </TableBody>
+      </Table>
+    </div>
+    <div class="flex items-center justify-end py-4 space-x-2">
+      <Button
+        variant="outline"
+        size="sm"
+        :disabled="!table.getCanPreviousPage()"
+        @click="table.previousPage()"
+      >
+        Previous
+      </Button>
+      <Button
+        variant="outline"
+        size="sm"
+        :disabled="!table.getCanNextPage()"
+        @click="table.nextPage()"
+      >
+        Next
+      </Button>
+    </div>
   </div>
 </template>
 <script setup lang="ts" generic="TData, TValue">
-import type { ColumnDef } from "@tanstack/vue-table";
-import { useVueTable, getCoreRowModel, FlexRender } from "@tanstack/vue-table";
+import type { ColumnDef, SortingState } from "@tanstack/vue-table";
+import {
+  useVueTable,
+  getCoreRowModel,
+  FlexRender,
+  getSortedRowModel,
+  getPaginationRowModel,
+} from "@tanstack/vue-table";
 import {
   Table,
   TableHeader,
@@ -57,10 +85,15 @@ import {
   TableCell,
 } from "@/components/ui/table";
 
+import { valueUpdater } from "@/lib/utils";
+import { ref } from "vue";
+
 const props = defineProps<{
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
 }>();
+
+const sorting = ref<SortingState>([]);
 
 const table = useVueTable({
   get data() {
@@ -70,6 +103,14 @@ const table = useVueTable({
     return props.columns;
   },
   getCoreRowModel: getCoreRowModel(),
+  getSortedRowModel: getSortedRowModel(),
+  onSortingChange: (updaterOrValue) => valueUpdater(updaterOrValue, sorting),
+  state: {
+    get sorting() {
+      return sorting.value;
+    },
+  },
+  getPaginationRowModel: getPaginationRowModel(),
 });
 </script>
 <style scoped></style>
---
+++ BLM_FRNT/src/components/users/UsersList.vue
@@ -0,0 +1,14 @@
+<template>
+  <GenericTable
+    :useQueryFn="useUsers"
+    :columns="userColumns"
+  />
+</template>
+
+<script setup lang="ts">
+import { useUsers } from "@/services/queries/useUsers";
+import GenericTable from "@components/GenericTable.vue";
+import { columns as userColumns } from "@/components/users/columns";
+</script>
+
+<style scoped></style>
