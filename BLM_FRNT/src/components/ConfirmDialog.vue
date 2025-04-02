2a7be07a6e9b172e11266bbe3b1af56ecb64e078
Clement-Mchar <clement.mchar@gmail.com>
2025-04-02 20:12:51 +0200

    added a confirm alert dialog

---
+++ BLM_FRNT/src/components/ConfirmDialog.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+defineEmits(["confirm"]);
+defineProps<{
+  title: string;
+  message: string;
+}>();
+</script>
+
+<template>
+  <AlertDialog>
+    <AlertDialogTrigger as-child><slot /></AlertDialogTrigger>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle> {{ title }}</AlertDialogTitle>
+        <AlertDialogDescription>
+          {{ message }}
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction @click="$emit('confirm')"
+          >Continue</AlertDialogAction
+        >
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</template>
