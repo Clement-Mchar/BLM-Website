8f3b368e4c5825a79e664b7b85d60f5de9efc18b
Clement-Mchar <clement.mchar@gmail.com>
2025-04-29 02:05:37 +0200

    added some queryOptions to my queryClient

--- BLM_FRNT/src/main.ts
+++ BLM_FRNT/src/main.ts
@@ -2,9 +2,16 @@ import { createApp } from "vue";
 import "@/index.css";
 import App from "@/App.vue";
 import router from "@/router/index";
-import { VueQueryPlugin } from "@tanstack/vue-query";
+import { VueQueryPlugin, type VueQueryPluginOptions } from "@tanstack/vue-query";
 
+const vueQueryPluginOptions: VueQueryPluginOptions = {
+  queryClientConfig: {
+    defaultOptions: { queries: { staleTime: 360000, retry:true,  }, },
+    
+  },
+  enableDevtoolsV6Plugin: true,
+}
 createApp(App)
   .use(router)
-  .use(VueQueryPlugin, { enableDevtoolsV6Plugin: true })
+  .use(VueQueryPlugin, vueQueryPluginOptions)
   .mount("#app");
