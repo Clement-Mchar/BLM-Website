be1e34ff044692b6a48958e6d0f0273ac321d766
Clement-Mchar <clement.mchar@gmail.com>
2025-05-05 21:18:40 +0200

    front: added shadcn-vue toaster component

--- BLM_FRNT/src/App.vue
+++ BLM_FRNT/src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { VueQueryDevtools } from "@tanstack/vue-query-devtools";
+
+import Toaster from "@/components/ui/toast/Toaster.vue";
 </script>
 
 <template>
+  <Toaster />
   <router-view />
   <VueQueryDevtools />
 </template>
