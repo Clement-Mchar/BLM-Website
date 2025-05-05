5acadec6cbf5fb8f21be0dfc2445653889da07ff
Clement-Mchar <clement.mchar@gmail.com>
2025-05-05 22:03:18 +0200

    reworked my seeder to create several user's instances for testing purpose

--- BLM_BCK/database/seeders/002_admin_seeder.ts
+++ BLM_BCK/database/seeders/002_admin_seeder.ts
@@ -6,13 +6,43 @@ import env from '#start/env'
 export default class extends BaseSeeder {
   async run() {
     const pwd = env.get('ADMIN_PASSWORD')
-    const admin = new User()
+    const users = await User.createMany([
+      {
+        username : "Don",
+        password : pwd as string,
+        userRole : UserRole.Admin,
+        isAdmin : true,
+      },
+      { username: "Milo", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Zoe", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Leo", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Ivy", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Nina", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Owen", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Eli", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Noa", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Liam", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Jade", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Max", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Amy", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Ezra", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Maya", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Hugo", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Nico", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Léa", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Theo", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Emma", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Ray", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Sia", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Yuna", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Tao", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Gus", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Ava", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Mira", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Kai", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Nils", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+      { username: "Zia", password: "aaaaaaaa", userRole: UserRole.Webmaster, isAdmin: false },
+    ])
 
-    admin.username = "Don"
-    admin.password = pwd as string;
-    admin.userRole = UserRole.Admin
-    admin.isAdmin = true
-
-    await admin.save()
   }
 }
