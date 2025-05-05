e06c068eff063e0ebfd6a2cd5a96745e3abeed64
Clement-Mchar <clement.mchar@gmail.com>
2025-05-05 22:06:30 +0200

    added a global filter, checkbox system and pagination to my entities lists / a dropdown confirmation dialog to my delete many button

--- BLM_FRNT/src/components/DataTable.vue
+++ BLM_FRNT/src/components/DataTable.vue
@@ -1,100 +1,24 @@
-<template>
-  <div class="flex flex-col">
-    <div
-      class="flex flex-col border-r-0 border-l-0 border-t-2 border-b-2 max-w-fit h-fit justify-center"
-    >
-      <Table>
-        <TableHeader>
-          <TableRow
-            v-for="headerGroup in table.getHeaderGroups()"
-            :key="headerGroup.id"
-          >
-            <TableHead
-              class="text-white"
-              v-for="header in headerGroup.headers"
-              :key="header.id"
-            >
-              <FlexRender
-                v-if="!header.isPlaceholder"
-                :render="header.column.columnDef.header"
-                :props="header.getContext()"
-              />
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <template v-if="table.getRowModel().rows?.length">
-            <TableRow
-              v-for="row in table.getRowModel().rows"
-              :key="row.id"
-              :data-state="row.getIsSelected() ? 'selected' : undefined"
-            >
-              <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
-                <FlexRender
-                  :render="cell.column.columnDef.cell"
-                  :props="cell.getContext()"
-                />
-              </TableCell>
-            </TableRow>
-          </template>
-          <template v-else>
-            <TableRow>
-              <TableCell :colspan="columns.length" class="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          </template>
-        </TableBody>
-      </Table>
-    </div>
-    <div class="flex items-center justify-end py-4 space-x-2">
-      <Button
-        variant="secondary"
-        size="sm"
-        :disabled="!table.getCanPreviousPage()"
-        @click="table.previousPage()"
-      >
-        Previous
-      </Button>
-      <Button
-        variant="secondary"
-        size="sm"
-        :disabled="!table.getCanNextPage()"
-        @click="table.nextPage()"
-      >
-        Next
-      </Button>
-    </div>
-  </div>
-</template>
-<script setup lang="ts" generic="TData, TValue">
+<script setup lang="ts" generic="TData extends { id: number }, TValue">
 import type { ColumnDef, SortingState } from "@tanstack/vue-table";
-import {
-  useVueTable,
-  getCoreRowModel,
-  FlexRender,
-  getSortedRowModel,
-  getPaginationRowModel,
-} from "@tanstack/vue-table";
-import {
-  Table,
-  TableHeader,
-  TableRow,
-  TableHead,
-  TableBody,
-  TableCell,
-} from "@/components/ui/table";
-import { Button } from "@/components/ui/button";
+import { useVueTable, getCoreRowModel, FlexRender, getSortedRowModel, getFilteredRowModel, getPaginationRowModel } from "@tanstack/vue-table";
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table";
+import Button from "~/components/Button.vue";
 import { valueUpdater } from "@/lib/utils";
-import { ref } from "vue";
-
+import { h, ref } from "vue";
+import { Trash2 } from "lucide-vue-next";
+import { useDeleteUsers } from "@/services/queries/useUsers";
+import ConfirmDialog from "./ConfirmDialog.vue";
+import { useToast } from "./ui/toast";
+import ToastAction from "./ui/toast/ToastAction.vue";
+import { Input } from "@/components/ui/input";
 const props = defineProps<{
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
 }>();
 
 const sorting = ref<SortingState>([]);
-
+const rowSelection = ref({});
+const globalFilter = ref("");
 const table = useVueTable({
   get data() {
     return props.data;
@@ -104,13 +28,98 @@ const table = useVueTable({
   },
   getCoreRowModel: getCoreRowModel(),
   getSortedRowModel: getSortedRowModel(),
+  getPaginationRowModel: getPaginationRowModel(),
+  onRowSelectionChange: (updaterOrValue) => valueUpdater(updaterOrValue, rowSelection),
+  getFilteredRowModel: getFilteredRowModel(),
+  initialState: {
+    pagination: {
+      pageIndex: 0,
+      pageSize: 10,
+    },
+  },
+  enableGlobalFilter: true,
   onSortingChange: (updaterOrValue) => valueUpdater(updaterOrValue, sorting),
   state: {
+    get globalFilter() {
+      return globalFilter.value;
+    },
     get sorting() {
       return sorting.value;
     },
+    get rowSelection() {
+      return rowSelection.value;
+    },
   },
-  getPaginationRowModel: getPaginationRowModel(),
+  onGlobalFilterChange: (updaterOrValue) => {
+    globalFilter.value = typeof updaterOrValue === "function" ? updaterOrValue(globalFilter.value) : updaterOrValue;
+  },
 });
+const { mutate: deleteUsers } = useDeleteUsers();
+
+const handleDeleteSelected = () => {
+  const { toast } = useToast();
+  const selectedIds = table.getFilteredSelectedRowModel().rows.map((row) => row.original.id);
+  deleteUsers(selectedIds);
+  toast({
+    title: "User deleted",
+    description: `Successfully deleted`,
+    action: h(
+      ToastAction,
+      {
+        altText: `deleted successfully`,
+      },
+      {
+        default: () => "Continue",
+      },
+    ),
+  });
+};
 </script>
+<template>
+  <div class="flex flex-col">
+    <div class="flex items-center py-4">
+      <Input :modelValue="globalFilter ?? ''" @update:modelValue="(value) => (globalFilter = String(value))" placeholder="Search all columns..." />
+    </div>
+    <div class="flex flex-col border-r-2 border-l-2 border-t-2 border-b-2 max-w-fit h-fit justify-center">
+      <Table>
+        <TableHeader>
+          <TableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
+            <TableHead class="text-white text-center" v-for="header in headerGroup.headers" :key="header.id">
+              <FlexRender v-if="!header.isPlaceholder" :render="header.column.columnDef.header" :props="header.getContext()" />
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <template v-if="table.getRowModel().rows?.length">
+            <TableRow v-for="row in table.getRowModel().rows" :key="row.id" :data-state="row.getIsSelected() ? 'selected' : undefined">
+              <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
+                <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
+              </TableCell>
+            </TableRow>
+          </template>
+          <template v-else>
+            <TableRow>
+              <TableCell :colspan="columns.length" class="h-24 text-center"> No results. </TableCell>
+            </TableRow>
+          </template>
+        </TableBody>
+      </Table>
+    </div>
+    <div class="flex flex-row justify-between">
+      <div>
+        <div class="text-white min-w-full align-center h-12 class= m-0 p-0">
+          <ConfirmDialog asChild title="Are you sure ?" message="This action cannot be undone" @confirm="handleDeleteSelected">
+            <Button v-if="table.getFilteredSelectedRowModel().rows.length > 0" type="button" class="mt-2 pl-[10px]"
+              ><Trash2 :stroke-width="1.5"
+            /></Button>
+          </ConfirmDialog>
+        </div>
+      </div>
+      <div class="flex items-center justify-end py-4 space-x-2">
+        <Button type="button" text="Before" :disabled="!table.getCanPreviousPage()" @click="table.previousPage()" />
+        <Button type="button" text="Next" :disabled="!table.getCanNextPage()" @click="table.nextPage()" />
+      </div>
+    </div>
+  </div>
+</template>
 <style scoped></style>
--- BLM_FRNT/src/components/DataTableDropDown.vue
+++ BLM_FRNT/src/components/DataTableDropDown.vue
@@ -1,14 +1,17 @@
 <template>
   <DropdownMenu v-model:open="isOpen">
     <DropdownMenuTrigger><Settings :size="22" class="mt-2" /> </DropdownMenuTrigger>
-    <DropdownMenuContent>
-      <ConfirmDialog asChild title="Are you sure ?" message="This action cannot be undone" @confirm="deleteUser">
-        <Button variant="destructive">Delete</Button>
-      </ConfirmDialog>
+    <DropdownMenuContent class="flex flex-col p-0 m-0 text-center items-center">
+      <div class="text-white min-w-full align-center bg-red-600 hover:bg-red-800 h-12 class= m-0 p-0">
+        <ConfirmDialog asChild title="Are you sure ?" message="This action cannot be undone" @confirm="deleteUser">
+          <div class="p-2 text-xl">Delete</div>
+        </ConfirmDialog>
+      </div>
+
       <DropdownMenuSeparator />
-      <DropdownMenuItem asChild>
+      <DropdownMenuItem asChild class="w-full p-0">
         <router-link :to="{ name: 'edit-user', params: { id: props.user.id } }">
-          <Button variant="ghost" class="w-full justify-start">Edit</Button>
+          <Button text="Edit" type="button" class="text-xl w-full" />
         </router-link>
       </DropdownMenuItem>
       <DropdownMenuSeparator />
@@ -22,19 +25,18 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSepara
 import { Settings } from "lucide-vue-next";
 import { ref } from "vue";
 import { useDeleteUser } from "@/services/queries/useUsers";
-import { Button } from "@/components/ui/button";
-import { useQueryClient } from "@tanstack/vue-query";
+
+import Button from "~/components/Button.vue";
 
 const props = defineProps<{ user: { id: number } }>();
 
-const deleteUserMutation = useDeleteUser();
-const queryClient = useQueryClient();
+const deleteUserMutation = useDeleteUser(props.user.id);
+
 const isOpen = ref(false);
 
 const deleteUser = () => {
   deleteUserMutation.mutate(props.user.id, {
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["users"] });
       isOpen.value = false;
     },
   });
