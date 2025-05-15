e15a7e38ae2b074a72a3fec995d505e08a8ad112
Clement-Mchar <clement.mchar@gmail.com>
2025-05-15 06:11:36 +0200

    added enums for post and video categories

--- BLM_BCK/app/enums.ts
+++ BLM_BCK/app/enums.ts
@@ -4,3 +4,20 @@ export const UserRole = {
 } as const
 
 export type UserRole = (typeof UserRole)[keyof typeof UserRole];
+
+export const VideoCategory = {
+  Clip: 'CLIP',
+  Interview: 'INTERVIEW',
+  Live: 'LIVE',
+  Anounce: 'ANOUNCE',
+} as const
+
+export type VideoCategory = (typeof VideoCategory)[keyof typeof VideoCategory];
+
+export const PostCategory = {
+  News: 'NEWS',
+  Spotlight: 'SPOTLIGHT',
+  Event: 'EVENT',
+} as const
+
+export type PostCategory = (typeof PostCategory)[keyof typeof PostCategory];
