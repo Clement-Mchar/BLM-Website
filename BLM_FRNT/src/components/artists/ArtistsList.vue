30867190b855a372d60ab23faff2555bfb41bc03
Clement-Mchar <clement.mchar@gmail.com>
2025-05-15 06:24:41 +0200

    added artists list

---
+++ BLM_FRNT/src/components/artists/ArtistsList.vue
@@ -0,0 +1,20 @@
+<template>
+  <GenericTable
+    :useQueryFn="useArtists"
+    :columns="artistColumns"
+    :onDeleteSelected="handleDeleteSelected"
+  />
+</template>
+
+<script setup lang="ts">
+import { useArtists, useDeleteArtists } from "@/services/queries/useArtists";
+import GenericTable from "@components/GenericTable.vue";
+import { columns as artistColumns } from "@/components/artists/columns";
+const { mutate: deleteArtists } = useDeleteArtists();
+const handleDeleteSelected = (ids: string[]) => {
+  
+  deleteArtists(ids);
+}
+</script>
+
+<style scoped></style>
