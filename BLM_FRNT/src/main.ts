83a291407c95733e722be1124cef6fea5c8f798a
Clement-Mchar <clement.mchar@gmail.com>
2025-05-21 13:38:03 +0200

    added ohvueicon to the app

--- BLM_FRNT/src/main.ts
+++ BLM_FRNT/src/main.ts
@@ -3,7 +3,9 @@ import "@/index.css";
 import App from "@/App.vue";
 import router from "@/router/index";
 import { VueQueryPlugin, type VueQueryPluginOptions } from "@tanstack/vue-query";
+import { OhVueIcon, addIcons } from "oh-vue-icons";
 
+import { LaStrikethroughSolid } from "oh-vue-icons/icons";
 const vueQueryPluginOptions: VueQueryPluginOptions = {
   queryClientConfig: {
     defaultOptions: { queries: { staleTime: 360000, retry:true,  }, },
@@ -11,7 +13,9 @@ const vueQueryPluginOptions: VueQueryPluginOptions = {
   },
   enableDevtoolsV6Plugin: true,
 }
+addIcons(LaStrikethroughSolid)
 createApp(App)
+  .component("v-icon", OhVueIcon)
   .use(router)
   .use(VueQueryPlugin, vueQueryPluginOptions)
   .mount("#app");
