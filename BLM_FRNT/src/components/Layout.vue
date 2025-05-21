9bea68f5a48c025088d2266d621b1ae79031e01a
Clement-Mchar <clement.mchar@gmail.com>
2025-05-21 13:46:47 +0200

    styling: modified classes to fix some display bugs

--- BLM_FRNT/src/components/Layout.vue
+++ BLM_FRNT/src/components/Layout.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="min-h-screen text-white font-resolve bg-gray-900 w-full flex flex-col"
+    class="h-full text-white font-resolve bg-gray-900 w-full flex flex-col"
   >
     <slot></slot>
   </div>
