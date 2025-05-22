78bde650d1216b22b9629715285c7b7ce073b07d
Clement-Mchar <clement.mchar@gmail.com>
2025-05-22 11:39:07 +0200

    backend: deleted video_url column from artists table, put spotify column down on the list bc it was on top and it was bothering me

--- BLM_BCK/database/migrations/1731966489787_artists.ts
+++ BLM_BCK/database/migrations/1731966489787_artists.ts
@@ -1,25 +1,24 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class Artist extends BaseSchema {
-    protected tableName = 'artists'
+  protected tableName = 'artists'
 
-    public async up () {
-        this.schema.createTable(this.tableName, (table) => {
-            table.string('spotify').nullable()
-            table.uuid('id').primary()
-            table.string('name').notNullable()
-            table.text('bio').nullable()
-            table.string('genre').nullable()
-            table.string('twitter').nullable()
-            table.json('avatar').nullable()
-            table.string('insta').nullable()
-            table.string('role').nullable()
-            table.json('videos_urls').nullable()
-            table.timestamps(true)
-        })
-    }
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary()
+      table.string('name').notNullable()
+      table.json('avatar').nullable()
+      table.text('bio').nullable()
+      table.string('genre').nullable()
+      table.string('twitter').nullable()
+      table.string('insta').nullable()
+      table.string('spotify').nullable()
+      table.string('role').nullable()
+      table.timestamps(true)
+    })
+  }
 
-    public async down () {
-        this.schema.dropTable(this.tableName)
-    }
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
 }
