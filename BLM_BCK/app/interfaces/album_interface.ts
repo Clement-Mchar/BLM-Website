62d5b70964a6c6d1be76f4ce90edab204e28b73a
Clement-Mchar <clement.mchar@gmail.com>
2025-05-26 20:59:44 +0200

    backend: created album interface and validator

---
+++ BLM_BCK/app/interfaces/album_interface.ts
@@ -0,0 +1,19 @@
+import type { Attachment } from '@jrmc/adonis-attachment/types/attachment'
+import { DateTime } from 'luxon'
+
+
+export interface CreateAlbumData {
+      name: string
+      date: DateTime
+      cover?: Attachment
+      link?: string
+      artistIds?: string[]
+}
+
+export interface UpdateAlbumData {
+      name?: string
+      date?: DateTime
+      cover?: Attachment
+      link?: string
+      artistIds?: string[]
+}
---
+++ BLM_BCK/app/validators/album_validators.ts
@@ -0,0 +1,27 @@
+import vine from '@vinejs/vine'
+import { DateTime } from 'luxon'
+
+const baseAlbumSchema = {
+  name: vine.string().minLength(1).maxLength(100),
+  date: vine.date().transform((date) => DateTime.fromJSDate(date)),
+  cover: vine
+    .file({
+      size: '20mb',
+      extnames: ['jpg', 'jpeg', 'png', 'webp', 'avif'],
+    })
+    .optional(),
+  link: vine.string().activeUrl().optional(),
+  artistIds: vine.array(vine.string().uuid()).minLength(1),
+}
+
+export const createAlbumValidator = vine.compile(vine.object(baseAlbumSchema))
+
+export const updateAlbumValidator = vine.compile(
+  vine.object({
+    name: baseAlbumSchema.name.optional(),
+    date: baseAlbumSchema.date.optional(),
+    cover: baseAlbumSchema.cover.optional(),
+    link: baseAlbumSchema.link.optional(),
+    artistIds: baseAlbumSchema.artistIds.optional(),
+  })
+)
