7fbb61b724304e0dfb34d54814f69064299cd934
Clement-Mchar <clement.mchar@gmail.com>
2025-05-26 21:01:04 +0200

    backend: added albums crud routes

--- BLM_BCK/start/routes.ts
+++ BLM_BCK/start/routes.ts
@@ -11,6 +11,7 @@ import router from '@adonisjs/core/services/router'
 import { throttle } from '#start/limiter'
 import { middleware } from '#start/kernel'
 
+const AlbumController = () => import('#controllers/album_controller')
 const UserController = () => import('#controllers/user_controller')
 const ArtistController = () => import('#controllers/artist_controller')
 const AuthController = () => import('#controllers/auth_controller')
@@ -25,5 +26,6 @@ router.post('users/delete-many', [UserController, 'destroyMany']);
 router.resource('users', UserController).use('*', middleware.auth());
 router.post('artists/delete-many', [ArtistController, 'destroyMany']);
 router.resource('artists', ArtistController).use('*', middleware.auth());
+router.resource('albums', AlbumController).use('*', middleware.auth());
 })
 .prefix('api')
